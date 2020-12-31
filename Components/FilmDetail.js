f8b66aedb105a9130f9135c16b6916f1c2f38ef1
zinebidrissijanati <zineb.idrissijanati1@usmba.ac.ma>
2020-12-31 10:10:21 +0100

    feat: add new components

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity, Share, Platform  } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity, Share, Platform } from 'react-native'
 import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
 import moment from 'moment'
 import numeral from 'numeral'
@@ -10,21 +10,21 @@ import EnlargeShrink from '../Animations/EnlargeShrink'
 
 class FilmDetail extends React.Component {
   static navigationOptions = ({ navigation }) => {
-     const { params } = navigation.state
-     // On accède à la fonction shareFilm et au film via les paramètres qu'on a ajouté à la navigation
-     if (params.film != undefined && Platform.OS === 'ios') {
-       return {
-           // On a besoin d'afficher une image, il faut donc passe par une Touchable une fois de plus
-           headerRight: <TouchableOpacity
-                           style={styles.share_touchable_headerrightbutton}
-                           onPress={() => params.shareFilm()}>
-                           <Image
-                             style={styles.share_image}
-                             source={require('../Images/ic_share.png')} />
-                         </TouchableOpacity>
-       }
-     }
- }
+    const { params } = navigation.state
+    // On accède à la fonction shareFilm et au film via les paramètres qu'on a ajouté à la navigation
+    if (params.film != undefined && Platform.OS === 'ios') {
+      return {
+        // On a besoin d'afficher une image, il faut donc passe par une Touchable une fois de plus
+        headerRight: <TouchableOpacity
+          style={styles.share_touchable_headerrightbutton}
+          onPress={() => params.shareFilm()}>
+          <Image
+            style={styles.share_image}
+            source={require('../Images/ic_share.png')} />
+        </TouchableOpacity>
+      }
+    }
+  }
 
   constructor(props) {
     super(props)
@@ -38,29 +38,29 @@ class FilmDetail extends React.Component {
 
   // Fonction pour faire passer la fonction _shareFilm et le film aux
   //paramètres de la navigation. Ainsi on aura accès à ces données au moment de définir le headerRight
-    _updateNavigationParams() {
-      this.props.navigation.setParams({
-        shareFilm: this._shareFilm,
-        film: this.state.film
-      })
+  _updateNavigationParams() {
+    this.props.navigation.setParams({
+      shareFilm: this._shareFilm,
+      film: this.state.film
+    })
+  }
+  componentDidMount() {
+    const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item => item.id === this.props.navigation.state.params.idFilm)
+    if (favoriteFilmIndex !== -1) {
+      this.setState({
+        film: this.props.favoritesFilm[favoriteFilmIndex]
+      }, () => { this._updateNavigationParams() })
+      return
     }
-    componentDidMount() {
-       const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item => item.id === this.props.navigation.state.params.idFilm)
-       if (favoriteFilmIndex !== -1) {
-         this.setState({
-           film: this.props.favoritesFilm[favoriteFilmIndex]
-         }, () => { this._updateNavigationParams() })
-         return
-       }
 
-       this.setState({ isLoading: true })
-       getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
-         this.setState({
-           film: data,
-           isLoading: false
-         }, () => { this._updateNavigationParams() })
-       })
-     }
+    this.setState({ isLoading: true })
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      }, () => { this._updateNavigationParams() })
+    })
+  }
 
   _displayLoading() {
     if (this.state.isLoading) {
@@ -78,22 +78,22 @@ class FilmDetail extends React.Component {
   }
 
   _displayFavoriteImage() {
-     var sourceImage = require('../Images/ic_favorite_border.png')
-     var shouldEnlarge = false // Par défaut, si le film n'est pas en favoris, on veut qu'au clic sur le bouton, celui-ci s'agrandisse => shouldEnlarge à true
-     if (this.props.favoritesFilm.findIndex(item => item.id === this.state.film.id) !== -1) {
-       sourceImage = require('../Images/ic_favorite.png')
-       shouldEnlarge = true // Si le film est dans les favoris, on veut qu'au clic sur le bouton, celui-ci se rétrécisse => shouldEnlarge à false
-     }
-     return (
-       <EnlargeShrink
-         shouldEnlarge={shouldEnlarge}>
-         <Image
-           style={styles.favorite_image}
-           source={sourceImage}
-         />
-       </EnlargeShrink>
-     )
-   }
+    var sourceImage = require('../Images/ic_favorite_border.png')
+    var shouldEnlarge = false // Par défaut, si le film n'est pas en favoris, on veut qu'au clic sur le bouton, celui-ci s'agrandisse => shouldEnlarge à true
+    if (this.props.favoritesFilm.findIndex(item => item.id === this.state.film.id) !== -1) {
+      sourceImage = require('../Images/ic_favorite.png')
+      shouldEnlarge = true // Si le film est dans les favoris, on veut qu'au clic sur le bouton, celui-ci se rétrécisse => shouldEnlarge à false
+    }
+    return (
+      <EnlargeShrink
+        shouldEnlarge={shouldEnlarge}>
+        <Image
+          style={styles.favorite_image}
+          source={sourceImage}
+        />
+      </EnlargeShrink>
+    )
+  }
 
 
   _displayFilm() {
@@ -103,7 +103,7 @@ class FilmDetail extends React.Component {
         <ScrollView style={styles.scrollview_container}>
           <Image
             style={styles.image}
-            source={{uri: getImageFromApi(film.backdrop_path)}}
+            source={{ uri: getImageFromApi(film.backdrop_path) }}
           />
           <Text style={styles.title_text}>{film.title}</Text>
           <TouchableOpacity
@@ -116,13 +116,13 @@ class FilmDetail extends React.Component {
           <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
           <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
           <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
-          <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
-              return genre.name;
-            }).join(" / ")}
+          <Text style={styles.default_text}>Genre(s) : {film.genres.map(function (genre) {
+            return genre.name;
+          }).join(" / ")}
           </Text>
-          <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
-              return company.name;
-            }).join(" / ")}
+          <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function (company) {
+            return company.name;
+          }).join(" / ")}
           </Text>
         </ScrollView>
       )
@@ -131,8 +131,8 @@ class FilmDetail extends React.Component {
   _shareFilm() {
     const { film } = this.state
     Share.share({ title: film.title, message: film.overview })
- }
- _displayFloatingActionButton() {
+  }
+  _displayFloatingActionButton() {
     const { film } = this.state
     if (film != undefined && Platform.OS === 'android') { // Uniquement sur Android et lorsque le film est chargé
       return (
@@ -145,7 +145,7 @@ class FilmDetail extends React.Component {
         </TouchableOpacity>
       )
     }
-}
+  }
 
 
   render() {
@@ -200,32 +200,32 @@ const styles = StyleSheet.create({
     margin: 5,
     marginBottom: 15
   },
-  default_text: {
+  default_text: {
     marginLeft: 5,
     marginRight: 5,
     marginTop: 5,
   },
-  favorite_image:{
+  favorite_image: {
     flex: 1,
     width: null,
     height: null
   },
   share_touchable_floatingactionbutton: {
-   position: 'absolute',
-   width: 60,
-   height: 60,
-   right: 30,
-   bottom: 30,
-   borderRadius: 30,
-   backgroundColor: '#e91e63',
-   justifyContent: 'center',
-   alignItems: 'center'
- },
- share_image: {
-   width: 30,
-   height: 30
- },
- share_touchable_headerrightbutton: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    right: 30,
+    bottom: 30,
+    borderRadius: 30,
+    backgroundColor: '#e91e63',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  share_image: {
+    width: 30,
+    height: 30
+  },
+  share_touchable_headerrightbutton: {
     marginRight: 8
   }
 })
